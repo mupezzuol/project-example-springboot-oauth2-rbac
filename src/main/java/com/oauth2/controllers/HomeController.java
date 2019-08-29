58ae6a7682dbfe6c7f0fb0704354492023931578
Murillo Pezzuol <murillo.pezzuol@kaspper.com>
2019-08-29 11:54:46 -0300

    add debug @slf4j

--- src/main/java/com/oauth2/controllers/HomeController.java
+++ src/main/java/com/oauth2/controllers/HomeController.java
@@ -12,12 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 
 @RequestMapping("/")
 @Api(tags="Home - Test", description="Test Request")
 @RestController
+@Slf4j
 public class HomeController {
-	
 	
 	@GetMapping()
 	@ResponseBody
@@ -29,6 +30,14 @@ public class HomeController {
 	@ApiOperation(value = "Test List String")
 	@GetMapping("/list")
 	public ResponseEntity<HashMap<String, String>> testList() {
+		
+		//Test using '@Slf4j' for Lombok
+		log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("An INFO Message");
+        log.warn("A WARN Message");
+        log.error("An ERROR Message");
+        
 		HashMap<String, String> values = new HashMap<>();
 		values.put("Campo1", "Valor1");
 		values.put("Campo2", "Valor2");
