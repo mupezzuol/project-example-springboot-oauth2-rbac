bf61a709893211fabf2828434581aceecc9ca50e
Murillo Pezzuol <murillopezzuol@hotmail.com>
2019-12-03 16:36:06 -0200

    review code;

--- src/main/resources/com/oatuh2/sql/insert_init.sql
+++ src/main/resources/com/oatuh2/sql/insert_init.sql
@@ -100,5 +100,7 @@ INSERT INTO public.tbl_user_roles (user_id, role_id) VALUES(1, 1);
 INSERT INTO public.tbl_user_roles (user_id, role_id) VALUES(2, 2);
 INSERT INTO public.tbl_user_roles (user_id, role_id) VALUES(3, 3);
 INSERT INTO public.tbl_user_roles (user_id, role_id) VALUES(4, 3);
+INSERT INTO public.tbl_user_roles (user_id, role_id) VALUES(4, 2);
+
 
 
